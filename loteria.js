ac9aa66291f1e9e7bfe7b426a32afb45ed1b08e7
cristian <you@example.com>
2022-09-21 00:05:52 -0300

    funciones

--- loteria.js
+++ loteria.js
@@ -1,81 +1,37 @@
-
-let sorteo = Math.ceil(Math.random() * (99 - 0) + 0);
-document.getElementById("numero").innerHTML = sorteo;
-
-
-   
-let uno = Math.ceil(Math.random() * (99-0) + 0);
-    document.write(uno)
-
-if(sorteo === uno){
-    document.getElementById("igual1").innerHTML="X"
-}else{
-    document.getElementById(1).innerHTML=uno;
-}   
-  
-  
-let dos = Math.ceil(Math.random() * (99-0) + 0);
-document.write(dos)
-if(sorteo === dos){
-    document.getElementById("igual2").innerHTML="X"
-}else{
-    document.getElementById(2).innerHTML=dos;
-}   
-
-let tres = Math.ceil(Math.random() * (99-0) + 0);
-document.write(tres)
-if(sorteo === tres){
-    document.getElementById("igual3").innerHTML="X"
-}else{
-    document.getElementById(3).innerHTML=tres;
-}   
  
+let sorteo = Math.ceil(Math.random() * (5 - 0) + 0); 
+document.getElementById("numero").innerHTML=sorteo;
 
-let cuatro = Math.ceil(Math.random() * (99-0) + 0);
-document.write(cuatro)
-if(sorteo === cuatro){
-    document.getElementById("igual4").innerHTML="X"
-}else{
-    document.getElementById(4).innerHTML=cuatro;
-}   
-  
-let cinco = Math.ceil(Math.random() * (99-0) + 0);
-document.write(cinco)
-if(sorteo === cinco){
-    document.getElementById("igual5").innerHTML="X"
-}else{
-    document.getElementById(5).innerHTML=cinco;
-}   
+let uno = Math.ceil(Math.random() * (5-0) + 0);
+document.getElementById("1").innerHTML=uno;
+
+let dos = Math.ceil(Math.random() * (5-0) + 0);
+document.getElementById("2").innerHTML=dos;
+
+let tres = Math.ceil(Math.random() * (5-0) + 0);
+document.getElementById("3").innerHTML=tres;
 
 
-let seis = Math.ceil(Math.random() * (99-0) + 0);
-document.write(seis)
-if(sorteo === seis){
-    document.getElementById("igual6").innerHTML="X"
-}else{
-    document.getElementById(6).innerHTML=seis;
-}   
 
 
-let siete = Math.ceil(Math.random() * (99-0) + 0);
-document.write(siete)
-if(sorteo === siete){
-    document.getElementById("igual7").innerHTML="X"
-}else{
-    document.getElementById(7).innerHTML=siete;
-}   
-
-
-let ocho = Math.ceil(Math.random() * (99-0) + 0);
-document.write(ocho)
-if(sorteo === ocho){
-    document.getElementById("igual8").innerHTML="X"
-}else{
-    document.getElementById(8).innerHTML=ocho;
+function carton1() {
+    if (sorteo === uno) {
+        document.getElementById("1").innerHTML = "X"
+    }
 }
+carton1()
 
+function carton2() {
+    if (sorteo === dos) {
+        document.getElementById("2").innerHTML = "X"
+    }
+}
+carton2()
 
-
-
-
+function carton3() {
+    if (sorteo === tres) {
+        document.getElementById("3").innerHTML = "X"
+    }
+}
+carton3()
 
