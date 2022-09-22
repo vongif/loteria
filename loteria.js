156c692ec1621779de42e80b502958a2f825ab30
cristian <you@example.com>
2022-09-22 08:04:15 -0300

    paso 2

--- loteria.js
+++ loteria.js
@@ -1,37 +1,54 @@
- 
+
+function sortear(){
+
 let sorteo = Math.ceil(Math.random() * (5 - 0) + 0); 
 document.getElementById("numero").innerHTML=sorteo;
-
 let uno = Math.ceil(Math.random() * (5-0) + 0);
 document.getElementById("1").innerHTML=uno;
-
 let dos = Math.ceil(Math.random() * (5-0) + 0);
 document.getElementById("2").innerHTML=dos;
-
 let tres = Math.ceil(Math.random() * (5-0) + 0);
 document.getElementById("3").innerHTML=tres;
-
-
+let cuatro = Math.ceil(Math.random() * (5-0) + 0);
+document.getElementById("4").innerHTML=cuatro;
 
 
 function carton1() {
-    if (sorteo === uno) {
-        document.getElementById("1").innerHTML = "X"
+    if (uno === sorteo) {
+        document.getElementById("igual1").innerHTML = "X"
+    }else{
+        document.getElementById("1").innerHTML=uno;
     }
 }
 carton1()
 
 function carton2() {
-    if (sorteo === dos) {
-        document.getElementById("2").innerHTML = "X"
+    if (dos === sorteo) {
+        document.getElementById("igual2").innerHTML = "X"
+    }else{
+        document.getElementById("1").innerHTML=dos 
     }
 }
 carton2()
 
 function carton3() {
-    if (sorteo === tres) {
-        document.getElementById("3").innerHTML = "X"
+    if (tres === sorteo) {
+        document.getElementById("igual3").innerHTML = "X"
+    }else{
+        document.getElementById("1").innerHTML=tres
     }
 }
 carton3()
+
+function carton4() {
+    if (cuatro === sorteo) {
+        document.getElementById("igual4").innerHTML = "X"
+    }else{
+        document.getElementById("1").innerHTML=cuatro;
+    }
+}
+carton4()
+}
+
+sortear()
 
